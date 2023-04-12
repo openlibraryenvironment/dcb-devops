d9fe81390a3e4749488ccccb1694ad779f90e969
Ian Ibbotson <ianibbo@gmail.com>
2023-04-12 16:25:25 +0100

    Initial checkin

---
+++ docker-compose/init.sql
@@ -0,0 +1,13 @@
+select 1;
+
+CREATE USER kc_reshare_dcb WITH PASSWORD 'kc_reshare_dcb';
+
+DROP DATABASE if exists kc_reshare_dcb;
+CREATE DATABASE kc_reshare_dcb;
+GRANT ALL PRIVILEGES ON DATABASE kc_reshare_dcb to kc_reshare_dcb;
+
+DROP DATABASE if exists reshare_dcb;
+CREATE DATABASE reshare_dcb;
+GRANT ALL PRIVILEGES ON DATABASE reshare_dcb to reshare_dcb;
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
