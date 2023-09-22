09dd841bf1b5cb5cd485a3ebfd4d0dc1773c6b8c
Ian Ibbotson <ianibbo@googlemail.com>
2023-09-22 10:30:00 +0100

    updated2 g

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -1,22 +1,115 @@
 #!/bin/bash
 
-curl -u "elastic:LrIrNpc9360J7xc1EgI5987q" -X DELETE "https://reshare-hub-es.libsdev.k-int.com/mobius-si"
+ES_PW=`kubectl get secret reshare-dcb-es-es-elastic-user -n dcb-uat -o go-template='{{.data.elastic | base64decode}}'`
+# ES_HOME="https://reshare-dcb-uat-es.sph.k-int.com/mobius-si"
+ES_HOME="https://reshare-dcb-uat-es.sph.k-int.com"
+ES_CREDS="elastic:$ES_PW"
 
-curl -u "elastic:LrIrNpc9360J7xc1EgI5987q" -X PUT "https://reshare-hub-es.libsdev.k-int.com/mobius-si" \
+echo Drop old
+curl -u "$ES_CREDS" -X DELETE "$ES_HOME/mobius-si"
+
+
+
+echo Create new
+curl -u "$ES_CREDS" -X PUT "$ES_HOME/mobius-si" \
       -H 'Content-Type: application/json' -d'{
   "mappings": {
     "properties": {
       "title": {
         "type":  "text",
-        "index_options": "freqs",
         "fields": {
           "keyword": {
             "type": "keyword",
             "ignore_above": 256   
           }
         }
+      },
+      "bibClusterId":{
+        "type": "text",
+        "fields": {
+          "keyword": {
+            "type": "keyword",
+            "ignore_above": 256   
+          }
+        }
+
+      },
+      "members": {
+        "properties": {
+          "bibId": {
+            "type": "keyword"
+          },
+          "sourceSystem": {
+            "type": "keyword"
+          }
+        }
+      },
+      "isbn":{
+        "type":  "text",
+        "fields": {
+          "keyword": {
+            "type": "keyword",
+            "ignore_above": 256   
+          }
+        }
+      },
+      "issn":{
+        "type":  "text",
+        "fields": {
+          "keyword": {
+            "type": "keyword",
+            "ignore_above": 256   
+          }
+        }
+      },
+      "metadata": {
+        "properties": {
+          "agents":{
+            "properties":{
+              "label":{
+                "type":"text",
+                "fields": {
+                  "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256   
+                  }
+                }
+              },
+              "subtype":{
+                "type":"keyword"
+              }
+            }
+          },
+          "subjects":{
+            "properties":{
+              "label":{
+                "type": "text",
+                "fields": {
+                  "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256   
+                  }
+                }
+              },
+              "subtype":{
+                "type": "keyword"
+              }
+            }
+          },
+          "identifiers":{
+            "properties":{
+              "value":{
+                "type": "keyword"
+              },
+              "namespace":{
+                "type": "keyword"
+              }
+            }
+          }
+        }
       }
     }
   }
-}
-'
+}'
+
+echo done
