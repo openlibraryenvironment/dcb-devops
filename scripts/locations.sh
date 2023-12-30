578b56a700acbe8d3b44c0aeca695764b2366420
Ian Ibbotson <ian.ibbotson@k-int.com>
2023-12-30 10:28:19 +0000

    improve locations script

--- scripts/locations.sh
+++ scripts/locations.sh
@@ -9,9 +9,11 @@ LOCATION_NS_UUID=`uuidgen --sha1 -n $DCB_ROOT_UUID --name Location`
 
 TOKEN=`./login`
 
+# Editable sheet: https://docs.google.com/spreadsheets/d/1P8f5GYSzfhG84n2f1RuDom6gmjgaW7a3grW_K2DKqY0/edit#gid=412322753
+
 export IMPORT_REFERENCE=`date +%Y-%m-%dT%H:%M:%S%z`
 export LN=0
-curl -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vTbJ3CgU6WYT4t5njNZPYHS8xjhjD8mevHVJK2oUWe5Zqwuwm_fbvv58hypPgDjXKlbr9G-8gVJz4zt/pub?gid=412322753&single=true&output=tsv" | while IFS="	" read AGENCY PICKUP DISPLAY PRINT DELIVERY LASTCOL
+curl -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vTbJ3CgU6WYT4t5njNZPYHS8xjhjD8mevHVJK2oUWe5Zqwuwm_fbvv58hypPgDjXKlbr9G-8gVJz4zt/pub?gid=412322753&single=true&output=tsv" | while IFS="	" read AGENCY PICKUP DISPLAY PRINT DELIVERY LAT LON LASTCOL
 do
   if [ $LN -eq 0 ]
   then
@@ -20,7 +22,7 @@ do
     AGENCY_UUID=`uuidgen --sha1 -n $AGENCIES_NS_UUID --name $AGENCY`
     LOCATION_UUID=`uuidgen --sha1 -n $LOCATION_NS_UUID --name $PICKUP`
     echo
-    echo processing: $AGENCY $PICKUP $DISPLAY $PRINT $DELIVERY - \"$AGENCY\"=$AGENCY_UUID \"$PICKUP\"=$LOCATION_UUID
+    echo processing: $AGENCY $PICKUP $DISPLAY $PRINT $DELIVERY lat=$LAT lon=$LON - \"$AGENCY\"=$AGENCY_UUID \"$PICKUP\"=$LOCATION_UUID
     echo
     echo "Posting without URL"
     curl -X POST "$TARGET/locations" -H "Content-Type: application/json"  -H "Authorization: Bearer $TOKEN" -d "{ 
@@ -30,8 +32,10 @@ do
       \"type\":\"PICKUP\",
       \"agency\":\"$AGENCY_UUID\",
       \"isPickup\":true,
-      \"printLabel\":\"PRINT\",
-      \"deliveryStops\":\"DELIVERY\",
+      \"printLabel\":\"$PRINT\",
+      \"deliveryStops\":\"$DELIVERY\",
+      \"latitude\": $LAT,
+      \"longitude\": $LON,
       \"importReference\": \"cli-$IMPORT_REFERENCE\"
     }"
     echo
