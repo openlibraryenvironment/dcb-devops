348d9eb30e27892dfe3711089a21a1e789531b9b
Ian Ibbotson <ian.ibbotson@k-int.com>
2024-01-21 19:20:15 +0000

    updated

--- oai/profile-folio-oai.groovy
+++ oai/profile-folio-oai.groovy
@@ -12,7 +12,10 @@ import groovy.json.JsonOutput
 import groovy.json.JsonBuilder;
 import groovy.json.JsonSlurper;
 
-def target='default'
+// def target='default'
+def target='cs00000int_0004'
+
+println("Profiling ${target}");
 
 // Read or set up config
 Map cfg = initialise();
@@ -36,6 +39,8 @@ try {
   process(folio, cfg[target]);
 }
 catch ( Exception e ) {
+  println("Problem ${e}");
+  e.printStackTrace()
   cfg[target].lastError = e.message
 }
 finally {
@@ -44,6 +49,7 @@ finally {
   updateConfig(cfg);
 }
 
+println("Exiting");
 System.exit(0)
 
 private Map initialise() {
@@ -71,52 +77,75 @@ private void updateConfig(Map cfg) {
 }
 
 private void process(HttpBuilder http, Map cfg) {
+
 	Map context = [
-		pageno:0
+		pageno:0,
+    totalElapsed:0
   ]
-  Map r = getPage(http, cfg, context);
-  println("Got response: ${r}");
+
+  Map r = null;
+	do { 
+		r = getPage(http, cfg, context, r?.resumption);
+		def rolling_average = context.totalElapsed / context.pageno;
+		println("Got response[${context.pageno}]: ${r.numRecords} in ${r.elapsed}ms - rolling average:${rolling_average}");
+		println("DATA,${context.pageno},${r.numRecords},${r.elapsed},${rolling_average},${context.highest_datestamp},${r.last_identifier_on_page}");
+	} while ( r.resumption?:'' != '' )
+
 }
 
 
-private Map getPage(HttpBuilder http, Map cfg, Map context) {
+private Map getPage(HttpBuilder http, Map cfg, Map context, String resumption) {
 	Map get_page_result = [
 		startTime: System.currentTimeMillis()
 	];
 
-  println("request OAI page");
+  println("request OAI page ${new Date()}");
   http.get {
 		request.contentType = 'text/xml'
 		request.accept = 'text/xml'
     request.uri.path = "/oai".toString()
-    request.uri.query = [
+
+		if ( resumption != null ) {
+      request.uri.query = [
+        'verb':'ListRecords',
+				'apikey':cfg.apikey,
+				'resumptionToken':resumption
+			]
+		}
+		else {
+      request.uri.query = [
         'verb':'ListRecords',
         'metadataPrefix':'marc21_withholdings',
 				'apikey':cfg.apikey
-    ]
-    // if ( since != null ) {
-    //   request.uri.query.since = since;
-    //   println("${base}/clusters?size:${pagesize}&since=${since}");
-    // }
-    // else {
-    //   println("${base}/clusters?size:${pagesize}");
-    // }
-
-    // request.accept='application/xml'
-    // request.headers['Authorization'] = 'Bearer '+token
+      ]
+		}
 
     response.success { FromServer fs, Object body ->
-      println("Got success response : ${fs.getContentType()}");
 			get_page_result.endTime=System.currentTimeMillis();
 			context.pageno++;
 			get_page_result.elapsed=get_page_result.endTime-get_page_result.startTime;
-			get_page_result.numRecords=0;
+			context.totalElapsed += get_page_result.elapsed
+			get_page_result.numRecords=body.ListRecords.record.size();
+			get_page_result.resumption=body.ListRecords.resumptionToken.text();
+
+			String highest_datestamp = null;
+			body.ListRecords.record.each { r ->
+				String this_datestamp = r.header.datestamp.toString();
+				if ( ( highest_datestamp == null ) || ( this_datestamp > highest_datestamp ) ) {
+					highest_datestamp = this_datestamp;
+				}
+				get_page_result.last_identifier_on_page = r.header.identifier
+			}
+
+			if ( ( context.highest_datestamp == null ) || ( highest_datestamp > context.highest_datestamp ) ) {
+				context.highest_datestamp = highest_datestamp;
+				println("updating highest datestamp to ${context.highest_datestamp}");
+			}
     }
     response.failure { FromServer fs, Object body ->
-      println("Get Page : Problem body:${body} fs:${fs} status:${fs.getStatusCode()}");
+      println("Get Page : Problem body at ${date}:${new String(body)} fs:${fs} status:${fs.getStatusCode()}");
     }
   }
 
-	println("Return");
   return get_page_result;
 }
