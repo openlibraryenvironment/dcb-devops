2a128877df1dba7d6b135073f88aee92f1068688
Ian Ibbotson <ian.ibbotson@k-int.com>
2024-01-30 20:40:36 +0000

    updated

--- oai/profile-folio-oai.groovy
+++ oai/profile-folio-oai.groovy
@@ -13,7 +13,8 @@ import groovy.json.JsonBuilder;
 import groovy.json.JsonSlurper;
 
 // def target='default'
-def target='cs00000int_0004'
+// def target='cs00000int_0004'
+def target='cs000000013'
 
 println("Profiling ${target}");
 
@@ -80,21 +81,42 @@ private void process(HttpBuilder http, Map cfg) {
 
 	Map context = [
 		pageno:0,
-    totalElapsed:0
+    totalElapsed:0,
+		identifiers: [:]
   ]
 
   Map r = null;
 	do { 
-		r = getPage(http, cfg, context, r?.resumption);
+		println("${new Date()} fetch page ${context.pageno}");
+		r = getPage(http, cfg, context, r?.resumption, context.pageno);
 		def rolling_average = context.totalElapsed / context.pageno;
 		println("Got response[${context.pageno}]: ${r.numRecords} in ${r.elapsed}ms - rolling average:${rolling_average}");
 		println("DATA,${context.pageno},${r.numRecords},${r.elapsed},${rolling_average},${context.highest_datestamp},${r.last_identifier_on_page}");
+		byte[] decoded_resumption = null;
+
+		if ( r.resumption ) {
+			decoded_resumption = java.util.Base64.getDecoder().decode(r.resumption.toString().getBytes());
+			List<String> components = Arrays.asList((new String(decoded_resumption)).split('&'));
+			// println("Decoded resumption: ${components}");
+			String expiration = components.find { it.startsWith('expir')};
+			String offset = components.find { it.startsWith('offset')};
+			println("Expiration: ${expiration}");
+			println("Offset: ${offset}");
+		}
+
+		if ( context.pageno % 100 == 0 ) {
+			println("Sleeping 5");
+			Thread.sleep(300000);
+		}
+		else
+			Thread.sleep(1000);
+
 	} while ( r.resumption?:'' != '' )
 
 }
 
 
-private Map getPage(HttpBuilder http, Map cfg, Map context, String resumption) {
+private Map getPage(HttpBuilder http, Map cfg, Map context, String resumption, Integer pageno) {
 	Map get_page_result = [
 		startTime: System.currentTimeMillis()
 	];
@@ -134,6 +156,13 @@ private Map getPage(HttpBuilder http, Map cfg, Map context, String resumption) {
 				if ( ( highest_datestamp == null ) || ( this_datestamp > highest_datestamp ) ) {
 					highest_datestamp = this_datestamp;
 				}
+				String record_identifier = r.header.identifier;
+				if ( context.identifiers[record_identifier] == null )
+					context.identifiers[record_identifier] = []
+				else
+					println("**DUPLICATE ID ${record_identifier} seen before: ${context.identifiers[record_identifier]}");
+				context.identifiers[record_identifier].add(pageno)
+				
 				get_page_result.last_identifier_on_page = r.header.identifier
 			}
 
